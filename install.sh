dbf33ae8d7b4e67775777db2168b771ac12cd6e1
Nalin.x.Linux <Nalin.x.Linux@gmail.com>
2013-10-05 00:42:34 +0530

    sudo update-menus

--- install.sh
+++ install.sh
@@ -6,17 +6,16 @@ echo "Please run this script as root"
 exit
 fi
 
-echo "================= Sharada Braille  Wrieter =======================
- Sharada braille writer is a six key approach to producing
+echo "Sharada Braille Writer : - SBW is a six key approach to producing
  printmaterials. Letters f, d, s, j, k, l represent 1 2 3 4 5 6 of the
  braille dots respectively. By pressing "f" and "s" together will
  produce letter "k" and like."
 
 
-echo "============ Checking and Installing dependencies ================"
+echo "Checking and Installing dependencies"
 apt-get -qq install python3-gi espeak python3-enchant
 
-echo "============ Checking and removing existing files ================"
+echo "Checking and removing existing files"
 if [ -d /usr/share/pyshared/sbw_2_0 ];
 then 
 rm -rf /usr/share/pyshared/sbw_2_0
@@ -42,8 +41,8 @@ rm /usr/share/applications/sharada-braille-writer-2.0.desktop
 echo "Removing icon .......................Ok"
 fi
 
-echo "==================== Copying new files ==========================="
-echo "Creating sbw_2_0 folder  ................OK"
+echo "Copying new files "
+echo "Creating sbw_2_0 folder  ............OK"
 mkdir /usr/share/pyshared/sbw_2_0
 echo "Copying dara ........................OK"
 cp -r data /usr/share/pyshared/sbw_2_0/
@@ -60,5 +59,6 @@ sudo chmod 755 /usr/share/applications/sharada-braille-writer-2.0.desktop
 ldconfig
 touch /usr/lib/python3/dist-packages/sbw_2_0/__init__.py
 sudo chmod -R 777 /usr/share/pyshared/sbw_2_0/
-echo "============ Compleated==========================================="
+sudo update-menus
+echo "Compleated"
 #sudo -u `whoami` sharada-braille-writer-2.0
