366eeb8b673c881f624dfefe1ad61bd44ac1d5cc
Nalin.x.Linux <Nalin.x.Linux@gmail.com>
2013-10-04 21:12:01 +0530

    version independent

--- install.sh
+++ install.sh
@@ -17,48 +17,48 @@ echo "============ Checking and Installing dependencies ================"
 apt-get install python3-gi espeak python3-enchant
 
 echo "============ Checking and removing existing files ================"
-if [ -d /usr/share/pyshared/sbw ];
+if [ -d /usr/share/pyshared/sbw_2_0 ];
 then 
-rm -rf /usr/share/pyshared/sbw
+rm -rf /usr/share/pyshared/sbw_2_0
 echo "Removing existing Data...............Ok"
 fi
 
-if [ -d /usr/lib/python3/dist-packages/sbw ];
+if [ -d /usr/lib/python3/dist-packages/sbw_2_0 ];
 then 
-rm -rf /usr/lib/python3/dist-packages/sbw
+rm -rf /usr/lib/python3/dist-packages/sbw_2_0
 echo "Removing existing source.............Ok"
 fi
 
 
-if [ -e /usr/bin/sharada-braille-writer ];
+if [ -e /usr/bin/sharada-braille-writer-2.0 ];
 then 
-rm /usr/bin/sharada-braille-writer
+rm /usr/bin/sharada-braille-writer-2.0
 echo "Removing bin ........................Ok"
 fi
 
-if [ -e /usr/share/applications/sharada-braille-writer.desktop ];
+if [ -e /usr/share/applications/sharada-braille-writer-2.0.desktop ];
 then 
-rm /usr/share/applications/sharada-braille-writer.desktop
+rm /usr/share/applications/sharada-braille-writer-2.0.desktop
 echo "Removing icon .......................Ok"
 fi
 
 echo "==================== Copying new files ==========================="
-echo "Creating sbw folder  ................OK"
-mkdir /usr/share/pyshared/sbw
+echo "Creating sbw_2_0 folder  ................OK"
+mkdir /usr/share/pyshared/sbw_2_0
 echo "Copying dara ........................OK"
-cp -r data /usr/share/pyshared/sbw/
+cp -r data /usr/share/pyshared/sbw_2_0/
 echo "Copying ui xml's ....................OK"
-cp -r ui /usr/share/pyshared/sbw/
+cp -r ui /usr/share/pyshared/sbw_2_0/
 echo "Copying source files ................OK"
-cp -r sbw /usr/lib/python3/dist-packages/
+cp -r sbw_2_0 /usr/lib/python3/dist-packages/
 echo "Copying starter .....................OK"
-cp sharada-braille-writer /usr/bin/
+cp sharada-braille-writer-2.0 /usr/bin/
 echo "Copying icon ........................OK"
-cp sharada-braille-writer.desktop /usr/share/applications/
-sudo chmod 755 /usr/share/applications/sharada-braille-writer.desktop
+cp sharada-braille-writer-2.0.desktop /usr/share/applications/
+sudo chmod 755 /usr/share/applications/sharada-braille-writer-2.0.desktop
 
 ldconfig
-touch /usr/lib/python3/dist-packages/sbw/__init__.py
-sudo chmod -R 777 /usr/share/pyshared/sbw/
+touch /usr/lib/python3/dist-packages/sbw_2_0/__init__.py
+sudo chmod -R 777 /usr/share/pyshared/sbw_2_0/
 echo "============ Compleated==========================================="
-#sudo -u `whoami` sharada-braille-writer
+#sudo -u `whoami` sharada-braille-writer-2.0
